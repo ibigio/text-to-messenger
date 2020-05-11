bc25610991c49e5f604ea152b8215efdb3d71e68
Ilan Bigio <ilanbigio@gmail.com>
2020-05-10 23:46:15 -0500

    Added simple message responding.

--- src/app.js
+++ src/app.js
@@ -3,11 +3,58 @@
 const
   express = require('express'),
   bodyParser = require('body-parser'),
+  request = require('request'),
   app = express().use(bodyParser.json());
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 });
+
+// Handles messages events
+function handleMessage(sender_psid, received_message) {
+  let response;
+
+  // Check if the message contains text
+  if (received_message.text) {    
+
+    // Create the payload for a basic text message
+    response = {
+      "text": `You sent the message: "${received_message.text}". Now send me an image!`
+    }
+  }  
+  
+  // Sends the response message
+  callSendAPI(sender_psid, response); 
+}
+
+// Handles messaging_postbacks events
+function handlePostback(sender_psid, received_postback) {
+
+}
+
+// Sends response messages via the Send API
+function callSendAPI(sender_psid, response) {
+  // Construct the message body
+  let request_body = {
+    "recipient": {
+      "id": sender_psid
+    },
+    "message": response
+  }
+
+  request({
+    "uri": "https://graph.facebook.com/v2.6/me/messages",
+    "qs": { "access_token": process.env.PAGE_ACCESS_TOKEN },
+    "method": "POST",
+    "json": request_body
+  }, (err, res, body) => {
+    if (!err) {
+      console.log('message sent!')
+    } else {
+      console.error("Unable to send message:" + err);
+    }
+  }); 
+}
 
 app.post('/webhook', (req, res) => {  
  
@@ -23,6 +70,16 @@ app.post('/webhook', (req, res) => {
       // will only ever contain one message, so we get index 0
       let webhook_event = entry.messaging[0];
       console.log(webhook_event);
+
+      let sender_psid = webhook_event.sender.id;
+
+      // handle event
+      if (webhook_event.message) {
+        handleMessage(sender_psid, webhook_event.message); 
+      } else if (webhook_event.postback) {
+        handlePostback(sender_psid, webhook_event);
+      }
+
     });
 
     // Returns a '200 OK' response to all requests
@@ -38,7 +95,7 @@ app.post('/webhook', (req, res) => {
 app.get('/webhook', (req, res) => {
 
   // Your verify token. Should be a random string.
-  let VERIFY_TOKEN = "blueberries"
+  let VERIFY_TOKEN = "gCj4CfPhjfcBkJyfM491SD16KX4Yr1d6"
     
   // Parse the query params
   let mode = req.query['hub.mode'];
